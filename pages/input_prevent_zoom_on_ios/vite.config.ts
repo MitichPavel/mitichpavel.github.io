c5568f9c2214f67a144ddb0cb079f05db4bd2dbf
Pavlo Mitich <mitich.pavel@gmail.com>
2024-03-04 10:20:17 +0100

    feat: Zoom page on focus input add base to vite.config

--- pages/input_prevent_zoom_on_ios/vite.config.ts
+++ pages/input_prevent_zoom_on_ios/vite.config.ts
@@ -4,6 +4,7 @@ import mkcert from 'vite-plugin-mkcert'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/input_prevent_zoom_on_ios/dist/',
   plugins: [
     svelte(),
     mkcert(),

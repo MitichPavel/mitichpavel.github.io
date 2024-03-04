f8b15c793f49b64f6acc9edd276b83d465d5db3d
Pavlo Mitich <mitich.pavel@gmail.com>
2024-03-04 10:47:09 +0100

    feat: update base

--- pages/input_prevent_zoom_on_ios/vite.config.ts
+++ pages/input_prevent_zoom_on_ios/vite.config.ts
@@ -4,7 +4,7 @@ import mkcert from 'vite-plugin-mkcert'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/input_prevent_zoom_on_ios/dist/',
+  base: "./",
   plugins: [
     svelte(),
     mkcert(),
